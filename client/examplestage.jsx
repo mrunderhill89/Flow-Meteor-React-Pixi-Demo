fc5272420bab15c9038fd19bf92533206c71d6cb
Kevin <kevin@kevin-Gateway-Viper-C.(none)>
2015-05-08 19:45:02 -0700

    Got sprites working! (Art is from We're Screwed)

--- client/examplestage.jsx
+++ client/examplestage.jsx
@@ -1,13 +1,20 @@
+function assetpath(filename){
+    return filename;
+}
+
 ExampleStage = React.createClass({
   displayName: 'ExampleStage',
   render: function() {
     var fontstyle = {font:'40px Times'};
     var Stage = ReactPIXI.Stage;
     var Text = ReactPIXI.Text;
+    var Sprite = ReactPIXI.Sprite;
     return <Stage width={this.props.width} height={this.props.height}>
         <Text text="Hello Meteor!" x={this.props.x} y={this.props.y} style={fontstyle} anchor={new PIXI.Point(0.5,0)} key="2" />
         <Text text="Hello React!" x={this.props.x+50} y={this.props.y+50} style={fontstyle} anchor={new PIXI.Point(0.5,0)} key="3" />
-        <Text text="Hello Pixi!" x={this.props.x+100} y={this.props.y+100} style={fontstyle} anchor={new PIXI.Point(0.5,0)} key="4" />
+        <Text text="Hello Pixi!" x={this.props.x+100} y={this.props.y+100} style={fontstyle} anchor={new PIXI.Point(0.5,0)}
+        key="4" />
+        <Sprite image={assetpath('dragon_brain.png')} x={this.props.x} y={this.props.y} />
     </Stage>;
   }
 });
